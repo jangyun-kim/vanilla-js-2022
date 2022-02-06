145e8633ed1888828df802f275328d749eac4296
jangyun-kim <97277404+jangyun-kim@users.noreply.github.com>
2022-02-06 23:29:28 +0900

    Create weather & all set

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "caedd478e3edd74d4d411a5168cbee52";
+
+function onGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} ${data.main.temp}℃ @`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
